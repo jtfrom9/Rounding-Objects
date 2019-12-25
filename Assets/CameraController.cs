48ed18819d85d0550776a75f98500e94cd0eeca5
Jun Tachikawa <jtachikawa@xevo.com>
2019-12-25 09:26:45 +0900

    add CameraController

---
+++ Assets/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private GameObject mainCamera;
+    public float rotateSpeed = 2.0f;
+
+    void Start()
+    {
+        mainCamera = Camera.main.gameObject;
+    }
+
+    void Update()
+    {
+#if UNITY_EDITOR
+        rotateCamera();
+#endif
+    }
+
+    private void rotateCamera()
+    {
+        if(!Input.GetKey(KeyCode.Mouse0)) {
+            return;
+        }
+
+        Vector3 angle = new Vector3(Input.GetAxis("Mouse X") * rotateSpeed, Input.GetAxis("Mouse Y") * rotateSpeed, 0);
+        //transform.RotateAround()をしようしてメインカメラを回転させる
+        mainCamera.transform.RotateAround(Vector3.zero, Vector3.up, angle.x);
+        mainCamera.transform.RotateAround(Vector3.zero, transform.right, angle.y);
+    }
+}
