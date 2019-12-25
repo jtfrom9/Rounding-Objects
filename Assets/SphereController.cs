fc6a39ac9684eebd6e2c83a2b8a41e978cea661a
Jun Tachikawa <jtachikawa@xevo.com>
2019-12-25 09:40:43 +0900

    add Color change logic

--- Assets/SphereController.cs
+++ Assets/SphereController.cs
@@ -8,8 +8,31 @@ public class SphereController : MonoBehaviour
     public float speed;
     public Vector3 axis;
 
+    private Material material;
+    private bool visible = false;
+    private float time;
+
+    void Start()
+    {
+        material = this.GetComponent<Renderer>().material;
+    }
+
     void Update()
     {
         transform.RotateAround(Vector3.zero, axis, speed);
+        if (visible)
+        {
+            time += Time.deltaTime;
+            if (time > 5.0)
+            {
+                material.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+            }
+        }
+    }
+
+    void OnBecameVisible()
+    {
+        visible = true;
+        time = Time.deltaTime;
     }
 }
